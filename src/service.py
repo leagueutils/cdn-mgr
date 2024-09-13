95664bccb60ca83d4ee0d5d18bf70c405858e935
lukasthaler <area.zweiundfuenfzig@gmail.com>
2024-09-13 14:45:52 +0200

    chore: some cleanup and fixes

--- src/service.py
+++ src/service.py
@@ -2,13 +2,14 @@ import os
 import uuid
 
 import aiofiles
-from media_classes import MediaClass
 from subroutines import _remove_media, config, db
 
 from leagueutils.components.logging import get_logger
 from leagueutils.components.mesh import MessageService, routes
-from leagueutils.errors import CDNException, NotFoundException
+from leagueutils.errors import DbNotFoundException, MediaNotFound
 from triggers import CronTrigger
+
+from .media_classes import MediaClass
 
 mesh = MessageService()
 logger = get_logger()
@@ -27,11 +28,11 @@ async def add_media(media_bytes: bytes, media_class: str, filename: str):
     try:
         [media_id] = await db.fetchrow('SELECT media_id FROM cdn.media WHERE hash=$1', media_hash)
         store = False
-    except NotFoundException:
+    except DbNotFoundException:
         store = True
         media_id = str(uuid.uuid4())
 
-    name, extension = filename.split('.')[-1]
+    name, extension = filename.split('.')
     fp = medium.get_storage_path(base_path=config.base_path, media_id=media_id, extension=extension)
     if store:
         async with aiofiles.open(fp, 'wb+') as outfile:
@@ -72,7 +73,7 @@ async def retrieve_media(media_class: str, filename: str) -> bytes:
         async with aiofiles.open(symlink, 'rb') as infile:
             return await infile.read()
     except OSError as e:
-        raise CDNException('No such file') from e
+        raise MediaNotFound('No such file') from e
 
 
 @CronTrigger(cron_schedule='0 3 * * *', on_startup=False, logger=logger)  # run at 3:00 AM every day
@@ -82,7 +83,7 @@ async def cleanup_expired_assets():
             """SELECT m.media_class, l.link from cdn.links l JOIN cdn.media m USING(media_id)
             WHERE l.ttl < CURRENT_TIMESTAMP"""
         )
-    except NotFoundException:
+    except DbNotFoundException:
         return  # nothing to clean up
 
     for media_class, link in to_remove:
